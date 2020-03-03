3525f6ad4495a96487e2c96e50cbe622ef062bef
Archer <archer102125220.2015@gmail.com>
2020-03-03 18:05:13 +0800

    mssql migrations and seed clear

--- script/models/config/database.js
+++ script/models/config/database.js
@@ -2,20 +2,30 @@
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 
+//https://github.com/sequelize/cli/issues/766
+
 const envConfig = {
   dialect: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  port: parseInt(process.env.DB_PORT),
+
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+
+  operatorsAliases: false,
   define: {
     charset: process.env.DB_DEFINE_CHARSET,
     collate: process.env.DB_DEFINE_COLLATE,
   },
+
   dialectOptions: {
+    dialect: process.env.DB_CONNECTION,
     charset: process.env.DB_DIALECT_OPTIONS_CHARSET,
     collate: process.env.DB_DIALECT_OPTIONSE_COLLATE,
+    instanceName: process.env.DB_SERVER_NAME,
+    enableArithAbort: false,
+    encrypt: true,
   }
 };
 
--- script/models/migrations/00000-fix-db-charset.js
+++ script/models/migrations/00000-fix-db-charset.js
@@ -2,12 +2,13 @@
 
 export default {
     up: (queryInterface, Sequelize) => {
-        const { database, dialectOptions } = queryInterface.sequelize.config;
+        const { database, dialectOptions, dialect } = queryInterface.sequelize.config;
+        const { charset } = dialectOptions, { collate } = dialectOptions;
+        const mySql = `ALTER DATABASE ${database}
+      CHARACTER SET ${charset || 'utf8'} COLLATE ${collate || 'utf8_general_ci'};`;
+        const msSql = `SELECT 11`;
 
-        return queryInterface.sequelize.query(
-            `ALTER DATABASE ${database}
-          CHARACTER SET ${dialectOptions.charset} COLLATE ${dialectOptions.collate};`
-        )
+        return queryInterface.sequelize.query(dialect === 'mysql' ? mySql : msSql);
     },
     down: (queryInterface, Sequelize) => { }
 }

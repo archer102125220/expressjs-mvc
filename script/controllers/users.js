f3ca4088eba96b1697ea02203e2780a00f16e43b
Archer <archer102125220.2015@gmail.com>
2020-06-14 18:36:08 +0800

    reconstruction

--- script/controllers/users.js
+++ script/controllers/users.js
@@ -4,14 +4,18 @@ class Users {
     usersList = async (req, res, next) => {
         //const { id, start, end } = req.body; //→接受前端來的資料
         const userData = await UserService.AllUsers();
-
+        if ((userData || []).length === 0) {
+            res.status(200).json('查無資料');
+        }
         res.status(200).json(userData);
     }
 
     findUser = async (req, res, next) => {
         const { name } = req.params; //→接受URL上的資料(ex:/api/users/account/:name)
         const userData = await UserService.findUser({account:name});
-
+        if ((userData || []).length === 0) {
+            res.status(200).json('查無資料');
+        }
         res.status(200).json(userData);
     }
 
--- script/services/userService.js
+++ script/services/userService.js
@@ -4,7 +4,7 @@ const { Op } = sequelize;
 
 class userService {
     AllUsers = async (payload) => {
-        const userData = await userList.findAll({
+        return await userList.findAll({
             attributes: {
                 // include: [],  //外來鍵欄位
                 exclude: ['password']  //不顯示欄位
@@ -14,27 +14,15 @@ class userService {
         //     where: { id: 1 },  // where 條件
         //     attributes: ['id']  //指定回傳欄位
         // });
-
-        if (userData === null) {
-            return '查無資料';
-        }
-
-        return userData;
     }
     findUser = async (payload = {}) => {
-        const userData = await userList.findAll({
+        return await userList.findAll({
             where: payload, // where 條件
             attributes: {
                 // include: [],  //外來鍵欄位
                 exclude: ['password']  //不顯示欄位
             }
         });
-
-        if ((userData || []).length === 0) {
-            return '查無資料';
-        }
-
-        return userData;
     }
     createUser = async (payload = {}) => {
         const { account, password, email } = payload;

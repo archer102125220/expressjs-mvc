4302b28e38cfd5ab0adf56f75a570473dcb88a0d
Archer <archer102125220.2015@gmail.com>
2020-06-26 19:24:38 +0800

    Reconstruction

--- script/bin/wwwboot.js
+++ script/bin/wwwboot.js
@@ -41,9 +41,9 @@ if (process.env.HTTP) {
 if (process.env.HTTPS) {
   //https://medium.com/@savemuse/node-js-%E5%BB%BA%E7%AB%8Bhttps%E4%BC%BA%E6%9C%8D%E5%99%A8-46442e9cd433
   //https://dev.twsiyuan.com/2017/10/openssl-unable-to-load-config.html
-  const privateKey = fs.readFileSync(__dirname + '/sslcert/server-key.pem', 'utf8');
-  const certificate = fs.readFileSync(__dirname + '/sslcert/server-cert.pem', 'utf8');
-  const ca = fs.readFileSync(__dirname + '/sslcert/cert.pem', 'utf8');
+  const privateKey = fs.readFileSync(__dirname + '/../../sslcert/server-key.pem', 'utf8');
+  const certificate = fs.readFileSync(__dirname + '/../../sslcert/server-cert.pem', 'utf8');
+  const ca = fs.readFileSync(__dirname + '/../../sslcert/cert.pem', 'utf8');
   const credentials = { key: privateKey, cert: certificate, ca, passphrase: '??' };
   const httpsServer = https.createServer(credentials, App);
   if (Socket.state !== 'connected' && Socket.state !== 'connection') {
@@ -129,8 +129,14 @@ function onListening(services) {
 }
 
 function ipLog(request, response){
-  console.log(`a user from ${request.connection.remoteAddress}:${request.connection.remotePort}`);
-  //console.log(request.headers["x-forwarded-for"],request.headers["X-Forwarded-Port"]);
-  //console.log(request.headers);
+  console.log('-------------ip log---------------');
+  console.log('a user from :');
+  console.log(`${request.connection.remoteAddress}:${request.connection.remotePort}`);
+  //console.log('connection:');
   //console.log(request.connection);
+  //console.log(request.headers["x-forwarded-for"],request.headers["X-Forwarded-Port"]);
+  console.log('headers:');
+  console.log(request.headers);
+  //console.log(request.connection);
+  console.log('-------------ip log end---------------');
 }

809428e9e4d2b61fe925c17f5756dce0a292df1e
Archer <archer102125220.2015@gmail.com>
2020-07-01 23:29:52 +0800

    error message update

--- script/app.js
+++ script/app.js
@@ -80,7 +80,10 @@ class App extends Express {
       }
       // set locals, only providing error in development
         res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        console.log(err.message);
+        //res.locals.error = req.app.get('env') === 'development' ? err : {};
+        console.log(err.status);
+        console.log(err.stack);
 
       // render the error page
       res.status(err.status || 500);
